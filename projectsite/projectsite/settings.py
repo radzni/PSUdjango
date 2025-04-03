bb5f3209c2521ce846e9660f78ad9656ee804e2e
radz <20228004@psu.palawan.edu.ph>
2025-04-03 10:05:57 +0800

    maam username po ginamit na address sa pythonanywhere

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u#qq(924$j2-m87!z@^-ha8ed89(f*(0e)8i)9dj3an-)ay997
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psuspehere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['psuspehere.pythonanywhere.com', 'hassanradzni.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
